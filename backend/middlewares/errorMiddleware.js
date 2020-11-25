6ed78b1c98e1a3a340339f23d8a2f988d9c57b31
Parminder <iparm1996@gmail.com>
2020-11-25 14:17:46 -0500

    Added Backend Authentication

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,12 +1,16 @@
 const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
-  res.json({ message: '404 Error' });
+  next(error);
 };
 
-const errorHandler = (error, req, res, next) => {
-  const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
-  res.json({ message: error.message });
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 };
 
 export { notFound, errorHandler };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  resgisterUser,
+} from '../controllers/userController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/login', authUser);
+router.post('/', resgisterUser);
+
+router.route('/login').post(authUser);
+
+router.route('/profile').get(protect, getUserProfile);
+
 export default router;
